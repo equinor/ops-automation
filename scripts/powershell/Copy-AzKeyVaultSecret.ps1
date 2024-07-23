dac702e48d32fa2eb85c6a9a8f004c5beb36189b
Helena Solberg Kallekleiv <110530547+helenakallekleiv@users.noreply.github.com>
2024-07-23 10:08:16 +0200

    chore(scripts): rename parameter in accordance to linter error

--- scripts/powershell/Copy-AzKeyVaultSecret.ps1
+++ scripts/powershell/Copy-AzKeyVaultSecret.ps1
@@ -93,7 +93,7 @@ function aaLogin {
 }
 
 # Set and verify Azure Subscription Context
-function Set-Subscription {
+function Select-SubscriptionContext {
 
     param (
         [Parameter(Mandatory = $true)]
@@ -150,7 +150,7 @@ try {
     }
 
     # Set Context and make sure FireWall is set to "Allowed" to be able to access secrets
-    Set-Subscription -SubscriptionId $SubscriptionId
+    Select-SubscriptionContext -SubscriptionId $SubscriptionId
     $SourceVaultFirewall = Disable-KeyVaultFirewall -VaultName $SourceVaultName
     $TargetVaultFirewall = Disable-KeyVaultFirewall -VaultName $TargetVaultName
 
