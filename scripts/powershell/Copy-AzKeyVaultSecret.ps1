b8b7250d99d223d901a2e5e41b33c542d49c56d7
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-11-08 08:01:05 +0100

    wip

--- scripts/powershell/Copy-AzKeyVaultSecret.ps1
+++ scripts/powershell/Copy-AzKeyVaultSecret.ps1
@@ -6,7 +6,7 @@
     - RBAC as Key Vault Contributor for Source and Destination Vault
     - Read access policy for secrets at source Key Vault and Write access policy for secrets at destination Key Vault
 
-  .PARAMETER SourceVaultName
+  .PARAMETER VaultName
   Specifies the name of the source key vault.
 
   .PARAMETER TargetVaultName
@@ -20,6 +20,7 @@
 
   .PARAMETER SecretName
   Specifies the name of the secret to copy. You can as well specify multiple SecretNames for multiple secrets to copy.
+  Filters all secrets to the secrets of the specified names.
 
   .PARAMETER Force
   Forces the script to copy regardless if secret exist in target Key vault.
@@ -27,27 +28,21 @@
   .EXAMPLE
   This example shows how to copy all secrets from source to target vault within the same subscription.
   If secret exists in target Key vault, it will not be copied.
-  .\Copy-AzKeyVaultSecret.ps1 -SubscriptionId <String> -SourceVaultName <String> -TargetVaultName <String>
+  .\Copy-AzKeyVaultSecret.ps1 -SubscriptionId <String> -VaultName <String> -TargetVaultName <String>
 
   .EXAMPLE
   Similar to example above, this shows how to copy all secrets from source to target vault within the same subscription.
   Secret will be copied even if it already exist in target Key vault.
-  .\Copy-AzKeyVaultSecret.ps1 -SubscriptionId <String> -SourceVaultName <String> -TargetVaultName <String> -Force
+  .\Copy-AzKeyVaultSecret.ps1 -SubscriptionId <String> -VaultName <String> -TargetVaultName <String> -Force
 
   .EXAMPLE
   This example shows how to copy all secrets when vaults reside in different Azure Subscriptions:
-  .\Copy-AzKeyVaultSecret.ps1 -SubscriptionId <String> -TargetSubscriptionId <String> -SourceVaultName <String> -TargetVaultName <String>
+  .\Copy-AzKeyVaultSecret.ps1 -SubscriptionId <String> -TargetSubscriptionId <String> -VaultName <String> -TargetVaultName <String>
 #>
 
 param (
   [Parameter(Mandatory = $true)]
-  [string]$SubscriptionId,
-
-  [Parameter(Mandatory = $false)]
-  [string]$TargetSubscriptionId = $SubscriptionId,
-
-  [Parameter(Mandatory = $true)]
-  [string]$SourceVaultName,
+  [string]$VaultName,
 
   [Parameter(Mandatory = $true)]
   [string]$TargetVaultName,
@@ -55,54 +50,68 @@ param (
   [Parameter(Mandatory = $false)]
   [string[]]$SecretName,
 
-  # Force overwrite existing secrets
+  [Parameter(Mandatory = $false)]
+  [string]$SubscriptionId,
+
+  [Parameter(Mandatory = $false)]
+  [string]$TargetSubscriptionId = $SubscriptionId,
+
   [Parameter(Mandatory = $false)]
   [switch]$Force
 )
 
-$IpAddress = (Invoke-RestMethod -Uri "https://api.ipify.org")
+if ($SubscriptionId -ne "") {
+  Write-Information "Setting Azure context"
+  $Context = Set-AzContext -SubscriptionId $SubscriptionId
+}
+else {
+  Write-Information "Getting Azure context"
+  $Context = Get-AzContext
+}
+$SubscriptionName = $Context.Subscription.Name
+Write-Information "Subscription: $SubscriptionName ($SubscriptionId)"
+
+$IpAddress = Invoke-RestMethod "https://api.ipify.org"
 $IpAddressRange = "$IpAddress/32"
-Write-Information "Current IP address: $IpAddress"
+Write-Information "IP address: $IpAddress"
 
-$Context = Set-AzContext -Subscription $SubscriptionId
-Write-Information "Current subscription: $($Context.Subscription.Name)"
+$Vault = Get-AzKeyVault -VaultName $VaultName
+$AddNetworkRule = $Vault.NetworkAcls.IpAddressRanges -notcontains $IpAddressRange
 
-$SourceVault = Get-AzKeyVault -VaultName $SourceVaultName
-$AddNetworkRule = $SourceVault.NetworkAcls.IpAddressRanges -notcontains $IpAddressRange
+$Secrets = @()
 
 try {
   if ($AddNetworkRule) {
-    Write-Information "Add IP address to source Key vault"
-    $null = Add-AzKeyVaultNetworkRule -VaultName $SourceVaultName -IpAddressRange $IpAddressRange
+    Write-Information "Adding IP address range '$IpAddressRange' to Key Vault '$VaultName'"
+    $null = Add-AzKeyVaultNetworkRule -VaultName $VaultName -IpAddressRange $IpAddressRange
   }
 
-  Write-Information "Get list of secrets names"
-  $SourceVaultSecretNames = (Get-AzKeyVaultSecret -VaultName $SourceVaultName).Name
-
-  $SourceVaultSecrets = @()
-  $SourceVaultSecretNames | ForEach-Object {
-    $SourceVaultSecrets += (Get-AzKeyVaultSecret -VaultName $SourceVaultName -Name $_)
+  Write-Information "Getting secrets from Key Vault '$VaultName'"
+  (Get-AzKeyVaultSecret -VaultName $VaultName).Name | ForEach-Object {
+    $Secrets += (Get-AzKeyVaultSecret -VaultName $VaultName -Name $_)
   }
 }
 catch {
-  Write-Error "An error occurred: $($_.ErrorDetails)"
+  Write-Host "An error occurred:"
+  Write-Host $_
 }
 finally {
   if ($AddNetworkRule) {
-    Write-Information "Remove IP address from source Key vault"
-    $null = Remove-AzKeyVaultNetworkRule -VaultName $SourceVaultName -IpAddressRange $IpAddressRange
+    Write-Information "Removing IP address range '$IpAddressRange' from Key Vault '$VaultName'"
+    $null = Remove-AzKeyVaultNetworkRule -VaultName $VaultName -IpAddressRange $IpAddressRange
   }
 }
 
 if ($SecretName.Count -gt 0) {
-  Write-Information "Filtering to target secrets of specified names"
-  $SourceVaultSecrets = $SourceVaultSecrets | Where-Object { $_.Name -in $SecretName }
+  Write-Information "Filtering all secrets from Key Vault '$VaultName' to the secrets of the specified names"
+  $Secrets = $Secrets | Where-Object { $_.Name -in $SecretName }
 }
 
-Write-Information "If target subscription is specified, switch to target context"
 if ($TargetSubscriptionId -ne $SubscriptionId) {
-  $Context = Set-AzContext -Subscription $TargetSubscriptionId
-  Write-Information "Target subscription: $($Context.Subscription.Name)"
+  Write-Information "Setting Azure context"
+  $Context = Set-AzContext -SubscriptionId $TargetSubscriptionId
+  $SubscriptionName = $Context.Subscription.Name
+  Write-Information "Target subscription: $SubscriptionName ($SubscriptionId)"
 }
 
 $TargetVault = Get-AzKeyVault -VaultName $TargetVaultName
@@ -110,36 +119,30 @@ $AddNetworkRule = $TargetVault.NetworkAcls.IpAddressRanges -notcontains $IpAddre
 
 try {
   if ($AddNetworkRule) {
-    Write-Information "Add IP address to target Key vault"
+    Write-Information "Adding IP address range '$IpAddressRange' to target Key Vault '$TargetVaultName'"
     $null = Add-AzKeyVaultNetworkRule -VaultName $TargetVaultName -IpAddressRange $IpAddressRange
   }
 
-  # Copy secrets to target vault
-  $SourceVaultSecrets | ForEach-Object {
-    $TargetVaultSecretName = $_.Name
-    $TargetVaultSecret = Get-AzKeyVaultSecret -VaultName $TargetVaultName -Name $TargetVaultSecretName
+  foreach ($Secret in $Secrets) {
+    $TargetSecretName = $Secret.Name
+    $TargetSecret = Get-AzKeyVaultSecret -VaultName $TargetVaultName -Name $TargetSecretName
 
-    # Skip if secret exist in target vault
-    if ($TargetVaultSecret -eq $null -or $Force) {
-      $TargetVaultSecretExpDate = $_.Expires
-      $SecretValue = $_.SecretValue
-
-      # Add if secret does not exist, orparameter -Force is used
-      $Copy = Set-AzKeyVaultSecret -VaultName $TargetVaultName -Name $_.Name -Expires $TargetVaultSecretExpDate -SecretValue $SecretValue
-      Write-Information "Successfully replicated secret '$($Copy.Id)' "
+    if ($null -eq $TargetSecret -or $Force) {
+      Write-Information "Setting secret '$TargetSecretName' in target Key Vault '$TargetVaultName'"
+      $TargetSecret = Set-AzKeyVaultSecret -VaultName $TargetVaultName -Name $TargetSecretName -Expires $Secret.Expires -SecretValue $Secret.SecretValue
     }
     else {
-      # Secret already exists
-      Write-Information "Secret '$($_.Id)' already copied"
+      Write-Information "Secret '$TargetSecretName' already exists in target Key Vault '$TargetVaultName'"
     }
   }
 }
 catch {
-  Write-Error "An error occurred: $($_.ErrorDetails)"
+  Write-Host "An error occurred:"
+  Write-Host $_
 }
 finally {
   if ($AddNetworkRule) {
-    Write-Information "Remove IP address from target Key vault"
+    Write-Information "Removing IP address range '$IpAddressRange' from target Key Vault '$TargetVaultName'"
     $null = Remove-AzKeyVaultNetworkRule -VaultName $TargetVaultName -IpAddressRange $IpAddressRange
   }
 }
